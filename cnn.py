181df9e932524fd86ff9604d183f15063be9bef2
solidchrysalis <musicemail7171@gmail.com>
2025-04-15 01:18:36 -0400

    Updated README

--- cnn.py
+++ cnn.py
@@ -18,7 +18,7 @@ optim_name=sys.argv[2]
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 print(device)
 
-epochs = 50
+epochs = 5
 
 transform_cifar = transforms.Compose(
     [transforms.ToTensor(),
@@ -142,57 +142,3 @@ with torch.no_grad():
 print('Accuracy of the network on the 10000 test images: %d %%' % (
     100 * correct / total))
 
-
-"""
-class Net(nn.Module):
-    def __init__(self, num_classes=10):
-        super(Net, self).__init__()
-
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32 * 2, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=32 * 2, out_channels=64 * 2, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(in_channels=64 * 2, out_channels=64 * 2, kernel_size=3, padding=1)
-        self.conv4 = nn.Conv2d(in_channels=64 * 2, out_channels=128 * 2, kernel_size=3, padding=1)
-        self.conv5 = nn.Conv2d(in_channels=128 * 2, out_channels=128 * 2, kernel_size=3, padding=1)
-        self.conv6 = nn.Conv2d(in_channels=128 * 2, out_channels=128 * 2, kernel_size=3, padding=1)
-        self.conv7 = nn.Conv2d(in_channels=128 * 2, out_channels=256 * 2, kernel_size=3, padding=1)
-        self.conv8 = nn.Conv2d(in_channels=256 * 2, out_channels=256 * 2, kernel_size=3, padding=1)
-        self.conv9 = nn.Conv2d(in_channels=256 * 2, out_channels=256 * 2, kernel_size=3, padding=1)
-
-        self.bn1 = nn.BatchNorm2d(32 * 2)
-        self.bn2 = nn.BatchNorm2d(128 * 2)
-        self.bn3 = nn.BatchNorm2d(256 * 2)
-
-        self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.dropout = nn.Dropout2d(0.2)
-
-        self.fc1 = nn.Linear(4096 * 2, 4096 * 2)
-        self.fc2 = nn.Linear(4096 * 2, 2048 * 2)
-        self.fc3 = nn.Linear(2048 * 2, num_classes)
-        self.relu = nn.ReLU()
-
-    def forward(self, x):
-
-        x = self.relu(self.bn1(self.conv1(x)))
-        x = self.relu(self.conv2(x))
-        x = self.relu(self.conv3(x))
-        x = self.maxpool(x)
-
-        x = self.relu(self.bn2(self.conv4(x)))
-        x = self.relu(self.conv5(x))
-        x = self.relu(self.conv6(x))
-        x = self.maxpool(x)
-        x = self.dropout(x)
-
-        x = self.relu(self.bn3(self.conv7(x)))
-        x = self.relu(self.conv8(x))
-        x = self.relu(self.conv9(x))
-        x = self.maxpool(x)
-        x = self.dropout(x)
-
-        x = torch.flatten(x, start_dim=1)
-        x = self.relu(self.fc1(x))
-        x = self.relu(self.fc2(x))
-        x = self.dropout(x)
-        x = self.fc3(x)
-        return x
-"""
